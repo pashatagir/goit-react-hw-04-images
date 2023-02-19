5f909623981bc893895f6817f70586a742bc8c94
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2023-02-19 23:19:48 +0200

    commented class-component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,63 @@ import Loader from './Loader';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [showBtn, setShowBtn] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+    setIsLoading(true);
+    getImages(query, page)
+      .then(({ hits, totalHits }) => {
+        if (!hits?.length) {
+          toast.error(`Sorry, there are no images ${query}. Please try again.`);
+          return;
+        }
+        setImages(images => [...images, ...hits]);
+        if (page === 1) {
+          toast.success(`Hooray! We found ${totalHits} images`);
+        }
+
+        if (page > 1 && page >= Math.ceil(totalHits / 12)) {
+          toast.info(
+            `We're sorry, but you've reached the end of search "${query}". Please start a new search`
+          );
+        }
+        setShowBtn(page < Math.ceil(totalHits / 12));
+      })
+      .catch(() =>
+        toast.error(`Whoops, something went wrong! Please try again later!`)
+      )
+      .finally(() => setIsLoading(false));
+  }, [page, query]);
+
+  const handleFormSubmit = query => {
+    setQuery(query);
+    setPage(1);
+    setImages([]);
+  };
+
+  const onLoadMore = () => {
+    setPage(page => page + 1);
+  };
+
+  return (
+    <AppContainer>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery images={images} />
+      {isLoading && <Loader />}
+      {showBtn && <Button onClick={onLoadMore} />}
+      <ToastContainer />
+    </AppContainer>
+  );
+};
+
 // export class App extends Component {
 //   state = {
 //     query: '',
@@ -68,60 +125,3 @@ import 'react-toastify/dist/ReactToastify.css';
 //     );
 //   }
 // }
-
-export const App = () => {
-  const [query, setQuery] = useState('');
-  const [images, setImages] = useState([]);
-  const [page, setPage] = useState(1);
-  const [showBtn, setShowBtn] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (query === '') {
-      return;
-    }
-    setIsLoading(true);
-    getImages(query, page)
-      .then(({ hits, totalHits }) => {
-        if (!hits?.length) {
-          toast.error(`Sorry, there are no images ${query}. Please try again.`);
-          return;
-        }
-        setImages(images => [...images, ...hits]);
-        if (page === 1) {
-          toast.success(`Hooray! We found ${totalHits} images`);
-        }
-
-        if (page > 1 && page >= Math.ceil(totalHits / 12)) {
-          toast.info(
-            `We're sorry, but you've reached the end of search "${query}". Please start a new search`
-          );
-        }
-        setShowBtn(page < Math.ceil(totalHits / 12));
-      })
-      .catch(() =>
-        toast.error(`Whoops, something went wrong! Please try again later!`)
-      )
-      .finally(() => setIsLoading(false));
-  }, [page, query]);
-
-  const handleFormSubmit = query => {
-    setQuery(query);
-    setPage(1);
-    setImages([]);
-  };
-
-  const onLoadMore = () => {
-    setPage(page => page + 1);
-  };
-
-  return (
-    <AppContainer>
-      <Searchbar onSubmit={handleFormSubmit} />
-      <ImageGallery images={images} />
-      {isLoading && <Loader />}
-      {showBtn && <Button onClick={onLoadMore} />}
-      <ToastContainer />
-    </AppContainer>
-  );
-};
